bb1f16006c178cae2ec2cc18e068a9e488202d50
jacam666 <ja6cam@gmail.com>
2024-04-30 20:05:14 +0100

    fixed the non responding navbar on the contactus page

--- src/app/components/DonateButton.tsx
+++ src/app/components/DonateButton.tsx
@@ -5,7 +5,7 @@ export default function DonateButton() {
     return (
         <div className='text-center'>
             <a href='https://www.paypal.com' target='blank' rel='noopener noreferrer'>
-                <MDBBtn className='mt-4 mb-4 py-2 text-3xl'>Donate Now</MDBBtn>
+                <MDBBtn className='mt-4 mb-4 py-2' style={{ width: '40vw'}}>Donate Now</MDBBtn>
             </a>
         </div>
     );

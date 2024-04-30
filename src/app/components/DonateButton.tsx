62bc4e04f1eb4834b2f28aabfb130bdb9d45b9be
jacam666 <ja6cam@gmail.com>
2024-04-30 13:30:14 +0100

    added paypal link to the donate button

--- src/app/components/DonateButton.tsx
+++ src/app/components/DonateButton.tsx
@@ -4,7 +4,9 @@ import { MDBBtn } from 'mdb-react-ui-kit';
 export default function DonateButton() {
     return (
         <div className='text-center'>
-            <MDBBtn className='mt-4 mb-4 py-2 text-3xl'>Donate Now</MDBBtn>
+            <a href='https://www.paypal.com' target='blank' rel='noopener noreferrer'>
+                <MDBBtn className='mt-4 mb-4 py-2 text-3xl'>Donate Now</MDBBtn>
+            </a>
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,13 @@ import DonateButton from "./components/DonateButton";
 
 export default function Home() {
   return (
-    <div className=' bg-gradient-to-r from-blue-100 to-slate-600' style={{ minHeight: '100vh' }}>
+    <div className=' bg-gradient-to-r from-blue-200 to-slate-600' style={{ minHeight: '100vh' }}>
       <ResponsiveNavbar/>
       <div style={{ width: '100%' }}>
         <Image className="w-full" src="/images/WhatsApp Image 2024-04-27 at 17.10.27_0fadff63.jpg" alt="verde" width={1200}  height={100} style={{height: 'auto'}} />
       </div>
       <div className="text-black">
-        <h1 className="text-center text-4xl">Our Aim:</h1>
+        <h1 className="text-center text-5xl">Our Aim:</h1>
         <p className="px-3 text-center">We are a welfare charity aiming to help overcome poverty in Cape Verde and improve the quality of life for Cape Verdeans.</p>
       </div>
       <div style={{ width: '100%' }}>

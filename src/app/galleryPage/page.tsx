dfb79420e458abbc9a026107289d70f1e7b46799
jacam666 <ja6cam@gmail.com>
2024-05-01 16:07:56 +0100

    added temp images to the gallery page

--- src/app/galleryPage/page.tsx
+++ src/app/galleryPage/page.tsx
@@ -1,5 +1,58 @@
 export default function Gallery () {
     return (
-        <h1>Gallery</h1>
+        <div className="p-8 bg-white">
+            <h1 className="text-2xl font-bold text-black mb-4 text-center">Gallery</h1>
+            <div className="grid grid-cols-2 gap-4">
+                <div className="flex justify-center">
+                    <img src="images/OIP (2).jpeg" alt="Image 1" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="/images/R.jpeg" alt="Image 2" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="/images/R.png" alt="Image 3" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="/images/06.jpg" alt="Image 4" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="images/OIP (2).jpeg" alt="Image 1" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="/images/R.jpeg" alt="Image 2" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="/images/R.png" alt="Image 3" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="/images/06.jpg" alt="Image 4" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="images/OIP (2).jpeg" alt="Image 1" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="/images/R.jpeg" alt="Image 2" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="/images/R.png" alt="Image 3" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="/images/06.jpg" alt="Image 4" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="images/OIP (2).jpeg" alt="Image 1" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="/images/R.jpeg" alt="Image 2" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="/images/R.png" alt="Image 3" className="max-w-full" />
+                </div>
+                <div className="flex justify-center">
+                    <img src="/images/06.jpg" alt="Image 4" className="max-w-full" />
+                </div>
+            
+            </div>
+        </div>
     );
 }

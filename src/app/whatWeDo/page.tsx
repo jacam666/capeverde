6ff9c23bf1ea8f25b7c19b203a7a599b406a1abc
jacam666 <ja6cam@gmail.com>
2024-05-02 09:33:25 +0100

    added new images to what we do page

--- src/app/whatWeDo/page.tsx
+++ src/app/whatWeDo/page.tsx
@@ -16,18 +16,55 @@ export default function WhatWeDo() {
             <div className=' bg-white'>
                 <h1 className='text-center text-5xl py-3 text-black'>What we do</h1>
                 <div className='border border-gray-400  shadow-md py-2 mx-3'>
-                    <p className='px-4 text-black text-xl '>
+                    <p className='px-2 py-3 text-black text-xl '>
                         My Cape Verde aims to help as many families as possible living in poverty.<br />
                         We provide food parcels, funded by monetary donations and fund raising.<br /> We provide clothing ,
                         shoes and toiletries to families donated by tourists and our friends and families.<br />
+                    </p>
+                    <div className="grid grid-cols-2 gap-2">
+                        <div className="flex justify-center">
+                            <div className="image-container">
+                                <img src="/images/WhatsApp Image 2024-05-02 at 08.47_edited2.jpg" alt="Image 1" />
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <div className="image-container">
+                                <img src="/images/WhatsApp Image clothes donation1.jpg" alt="Image 2" />
+                            </div>
+                        </div>
+                    </div>
+                    <p className='px-2 py-2 text-black text-xl'>
                         We provide medical aids to people who cannot afford them and who are not assisted by the
                         health service of Cape Verde. <br />We provide wheelchairs and walking aids to locals with physical
                         disabilities.<br />
-                        We provide schools and children with stationery and school supplies.
+                        <div className="grid grid-cols-2 gap-2 pt-3">
+                            <div className="flex justify-center">
+                                <div className="image-container">
+                                    <img src="/images/WhatsApp Image wheelchair1.jpg" alt="Image 1" />
+                                </div>
+                            </div>
+                            <div className="flex justify-center">
+                                <div className="image-container">
+                                    <img src="/images/WhatsApp Image wheelchair2.jpg" alt="Image 2" />
+                                </div>
+                            </div>
+                        </div>
                     </p>
-                </div>
-                <div style={{ width: '100%' }}>
-                    <Image className='w-full px-3 py-3' src="/images/csm_Poverty_cap-vert1_3791a72817.jpg" alt='young child' width={500} height={500} />
+                    <p className='px-2 text-black text-xl'>
+                        We provide schools and children with stationery and school supplies.
+                        <div className="grid grid-cols-2 gap-2 pt-3">
+                            <div className="flex justify-center">
+                                <div className="image-container">
+                                    <img src="/images/WhatsApp Image school1.jpg" alt="Image 1" />
+                                </div>
+                            </div>
+                            <div className="flex justify-center">
+                                <div className="image-container">
+                                    <img src="/images/WhatsApp Image school2.jpg" alt="Image 2" />
+                                </div>
+                            </div>
+                        </div>
+                    </p>
                 </div>
                 <div className='border border-gray-400  shadow-sm py-2 mx-3'>
                     <p className='text-black px-4 text-xl py-3'>

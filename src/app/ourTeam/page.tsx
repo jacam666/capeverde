f33705979cf98fdbe5e8cc5f4d132eb3f3f9aae6
jacam666 <ja6cam@gmail.com>
2024-05-04 09:45:13 +0100

    adjusted the our team page to have name under image

--- src/app/ourTeam/page.tsx
+++ src/app/ourTeam/page.tsx
@@ -25,34 +25,37 @@ export default function Team() {
             img: "/images/team4.jpg",
         },
         {
-            title: "Name",
             img: "/images/team5.jpg",
+            title: "Name"
         },
     ];
 
     return (
-    <div className="bg-black">
-        <ResponsiveNavbar/>
-        <h1 className="text-white text-center mt-3">Meet Our Team</h1>
-        <div className="gap-2 grid  sm:grid-cols-2 grid-cols-1 ">
-            {list.map((item, index) => (
-                <Card key={index}>
-                    <CardBody className="h-auto">
-                        <Image
-                            radius="lg"
-                            width="100%"
-                            alt={item.title}
-                            className="w-full object-cover opacity-100"
-                            src={item.img}
-                        />
-                    </CardBody>
-                    <CardFooter className="text-lg">
-                        <b className="text-white">{item.title}</b>
-                        {/* <p className="text-default-500">{item.price}</p> */}
-                    </CardFooter>
-                </Card>
-            ))}
+        <div className="bg-black">
+            <ResponsiveNavbar />
+            <h1 className="text-white text-center mt-3">Meet Our Team</h1>
+            <div className="gap-2 grid  sm:grid-cols-2 grid-cols-1 ">
+                {list.map((item, index) => (
+                    <Card key={index}>
+                        <CardBody className="h-auto overflow-visible p-0">
+                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                <Image
+                                    radius="lg"
+                                    width="100%"
+                                    alt={item.title}
+                                    className="w-full object-cover opacity-100"
+                                    src={item.img}
+                                />
+                                <div className="text-center text-white">{item.title}</div>
+                            </div>
+                        </CardBody>
+                        {/* <CardFooter className="text-small justify-between">
+                            <b>{item.title}</b>
+                        </CardFooter> */}
+                    </Card>
+                ))}
+
+            </div>
         </div>
-    </div>
     );
 }

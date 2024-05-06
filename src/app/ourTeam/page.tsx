550514c904815f04119f9d71b026e7968d04df4f
jacam666 <ja6cam@gmail.com>
2024-05-06 19:30:32 +0100

    added volunteer profiles to meet the team page

--- src/app/ourTeam/page.tsx
+++ src/app/ourTeam/page.tsx
@@ -45,12 +45,51 @@ export default function Team() {
                                 <div className="text-center text-black text-3xl mt-3 ">{item.title}</div>
                             </div>
                         </CardBody>
-                        {/* <CardFooter className="text-small justify-between">
-                            <b>{item.title}</b>
-                        </CardFooter> */}
                     </Card>
                 ))}
-
+            </div>
+            <div className="border-top border-black mt-3">
+                <h1 className="text-center text-black mt-4">Our Volunteers</h1>
+                <div>
+                    <div className="grid sm:grid-cols-2 gap-3">
+                        <div className="my-3 mx-2">
+                            <img className="image-container" src="/images/volunteer1.jpg" alt="volunteer-1" />
+                            <h2 className="text-center text-black">Melissa</h2>
+                        </div>
+                        <div className="my-3 mx-2">
+                            <img className="image-container" src="/images/volunteer2.jpg" alt="volunteer2" />
+                            <h2 className="text-center text-black">Djack</h2>
+                        </div>
+                        <div className="my-3 mx-2">
+                            <img className="image-container" src="/images/volunteer3.jpg" alt="volunteer-3" />
+                            <h2 className="text-center text-black">Paulo</h2>
+                        </div>
+                        <div className="my-3 mx-2">
+                            <img className="image-container" src="/images/volunteer4.jpg" alt="volunteer-4" />
+                            <h2 className="text-center text-black">Ivan</h2>
+                        </div>
+                        <div className="my-3 mx-2">
+                            <img className="image-container" src="/images/volunteer5.jpg" alt="volunteer-5" />
+                            <h2 className="text-center text-black">Solange</h2>
+                        </div>
+                        <div className="my-3 mx-2">
+                            <img className="image-container" src="/images/volunteer6.jpg" alt="volunteer-6" />
+                            <h2 className="text-center text-black">Fernanda</h2>
+                        </div>
+                        <div className="my-3 mx-2">
+                            <img className="image-container" src="/images/volunteer7.jpg" alt="volunteer-6" />
+                            <h2 className="text-center text-black">Ivania</h2>
+                        </div>
+                        <div className="my-3 mx-2">
+                            <img className="image-container" src="/images/volunteer8.jpg" alt="volunteer-6" />
+                            <h2 className="text-center text-black">Marco</h2>
+                        </div>
+                        <div className="mt-3 mx-2">
+                            <img className="image-container" src="/images/volunteer9.jpg" alt="volunteer-6" />
+                            <h2 className="text-center text-black">Fortz</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );

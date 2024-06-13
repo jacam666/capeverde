010734d8e9f6698a87e7605dbc38638118fd8ea5
jacam666 <ja6cam@gmail.com>
2024-06-13 21:03:11 +0100

    added new styling to homepage

--- src/app/components/DonateButton.tsx
+++ src/app/components/DonateButton.tsx
@@ -5,7 +5,11 @@ export default function DonateButton() {
     return (
         <div className='text-center'>
             <a href='https://www.paypal.com' target='blank' rel='noopener noreferrer'>
-                <MDBBtn className='mt-4 mb-4 py-2 bg-gradient-to-br from-blue-500 via-white to-red-500 text-black' style={{ width: '40vw'}}>Donate Now</MDBBtn>
+                <MDBBtn className='mt-4 mb-4 py-2 bg-gradient-to-br from-blue-500 via-white to-red-500 text-black'
+                    style={{ width: '40vw' }}
+                >
+                    Donate Now
+                </MDBBtn>
             </a>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import ResponsiveNavbar from "./components/ResponsiveNavbar";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import Image from 'next/image';
 import Footer from "./components/Footer";
-import DonateButton from "./components/DonateButton";
+import HomePageButton from "./components/HomePageButton";
+import Image from "next/image";
+
 
 export const metadata = {
   title: "My Cape Verde"
@@ -11,22 +12,53 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className=' relative bg-gradient-to-br from-blue-500 via-white to-red-500' style={{ minHeight: '100vh'}}>
+    <div className='' >
       <ResponsiveNavbar />
-      <div className=" bg-gradient-to-r from-blue-500 via-white to-red-500">
-      <h1 className="text-3xl text-center pt-3">Our Aim:</h1>
-        <Image className="w-full px-3 py-3" src="/images/frontpageimage1.jpg" alt="verde" width={1200} height={100} style={{ height: 'auto' }} />
-        <div className="text-center text-black">
-          <p>We are a welfare charity aiming to help overcome poverty in Cape Verde and improve the quality of life for Cape Verdeans.</p>
+      <div className=" pb-8">
+        <div className="text-center">
+          <h1 className="homepage-header text-6xl text-gray-900 text-center mx-auto sm:w-1/2 pt-9">Building a Brighter Future for Cape Verde</h1>
+          <h3 className="text-xl mt-6">Join us in making a difference, one community at a time.</h3>
+        </div>
+        <HomePageButton />
       </div>
-      <div style={{ width: '100%' }}>
-        <Image className="w-full px-3" src="/images/Whats App Image 2024-04-27 at 17.10_edited.jpg" alt="verde" width={1200} height={100} style={{ height: 'auto' }} />
+      <h1 className="homepage-header text-5xl px-2 text-gray-900 text-center mx-auto sm:w-2/3 py-9">
+        We are a welfare charity aiming to help overcome poverty in Cape Verde and improve the quality of life for Cape Verdians.
+      </h1>
+      <div className="flex flex-col sm:flex-row md:justify-evenly ">
+        <div className="flex flex-col">
+          <p className="homepage-header text-lg font-bold text-gray-900 mt-8 px-4">Why Choose Us?</p>
+          <div className=" my-4 ml-6 h-1 mb-8 sm:h-1 w-2/6 lg:w-2/5 md:w-2/5 bg-black "></div>
+        </div>
+        <div className="flex flex-col sm:flex-row">
+          <div className="sm:w-1/2 my-3 flex flex-col justify-center items-center">
+            <Image
+              src="/images/frontpageimage1_edited.jpg"
+              alt="gallery"
+              width={500}
+              height={500}
+              className="homepage-image w-11/12"
+            />
+            <h5 className="flex text-lg text-black">
+              Community Development
+            </h5>
+          </div>
+          <div className="sm:w-1/2 flex flex-col justify-center items-center">
+            <Image
+              src="/images/Whats App Image 2024-04-27 at 17_edited2.jpg"
+              alt="gallery"
+              width={500}
+              height={500}
+              className="homepage-image w-11/12"
+            />
+            <h5 className="flex text-black text-lg">
+              Healthcare Services
+            </h5>
+          </div>
+        </div>
       </div>
-      <div>
-        <DonateButton />
-      </div>
+      
       <Footer />
-      </div>
     </div>
   );
 }
+

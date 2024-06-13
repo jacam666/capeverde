ecea7680e3214d09c34012615c8d990dadb4d44a
jacam666 <ja6cam@gmail.com>
2024-06-13 13:55:16 +0100

    fixed error in deployment

--- src/app/blogPage/page.tsx
+++ src/app/blogPage/page.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-interface BlogProps {
-    title: string;
-    content: string;
-}
+// interface BlogProps {
+//     title: string;
+//     content: string;
+// }
 
-const Blog: React.FC<BlogProps> = ({ title, content }) => {
-    return (
-        <div>
-            <h2>{title}</h2>
-            <h1>hello</h1>
-            <p>{content}</p>
-        </div>
-    );
-};
+// const Blog: React.FC<BlogProps> = ({ title, content }) => {
+//     return (
+//         <div>
+//             <h2>{title}</h2>
+//             <h1>hello</h1>
+//             <p>{content}</p>
+//         </div>
+//     );
+// };
 
-export default Blog;
+// export default Blog;

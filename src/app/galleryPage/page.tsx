ab0e839f5fa8adf02b4558eeffd8d54da5834093
jacam666 <ja6cam@gmail.com>
2024-06-13 14:48:46 +0100

    styled gallery

--- src/app/galleryPage/page.tsx
+++ src/app/galleryPage/page.tsx
@@ -13,7 +13,12 @@ const customStyles = {
         bottom: 'auto',
         marginRight: '-50%',
         transform: 'translate(-50%, -50%)',
+        backgroundColor: 'transparent',
+        border: 'none',
     },
+    overlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.75)'
+    }
 };
 
 export default function Gallery() {

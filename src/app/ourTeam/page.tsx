cbae50cc9314666aaa37499a2223ccea829eb388
jacam666 <ja6cam@gmail.com>
2024-06-14 20:46:02 +0100

    styled our team page

--- src/app/ourTeam/page.tsx
+++ src/app/ourTeam/page.tsx
@@ -28,66 +28,66 @@ export default function Team() {
     ];
 
     return (
-        <div className=" bg-gradient-to-r from-blue-500 via-white to-red-500">
+        <div className="">
             <ResponsiveNavbar />
-            <h1 className="text-black text-center mt-3">Meet Our Team</h1>
-            <div className="gap-2 grid  sm:grid-cols-2 grid-cols-1 ">
+            <h1 className="homepage-header text-7xl text-black text-center m-10">Meet Our Team</h1>
+            <div className="gap-1 grid  sm:grid-cols-2 xl:grid-cols-4 grid-cols-1 p-3">
                 {list.map((item, index) => (
                     <Card key={index}>
-                        <CardBody className="h-auto overflow-visible p-0 m-2">
+                        <CardBody className="h-auto  p-0 m-2 mb-5">
                             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                                 <Image
-                                    radius="lg"
-                                    width="100%"
+                                    radius="full"
+                                    width="60%"
                                     alt={item.title}
-                                    className="w-full object-cover opacity-100"
+                                    className="w-full object-cover rounded-full opacity-100"
                                     src={item.img}
                                 />
-                                <div className="text-center text-black text-3xl mt-3 ">{item.title}</div>
+                                <div className="text-center text-gray-800 text-4xl mt-3 ">{item.title}</div>
                             </div>
                         </CardBody>
                     </Card>
                 ))}
             </div>
             <div className="border-top border-black mt-3">
-                <h1 className="text-center text-black mt-4">Our Volunteers</h1>
+                <h1 className="homepage-header text-7xl text-black text-center m-10">Our Volunteers</h1>
                 <div>
-                    <div className="grid sm:grid-cols-2 gap-3">
+                    <div className="grid sm:grid-cols-2 xl:grid-cols-4 gap-1 over">
                         <div className="my-3 mx-2">
-                            <img className="image-container" src="/images/volunteer1.jpg" alt="volunteer-1" />
-                            <h2 className="text-center text-black">Melissa</h2>
+                            <img className="image-team rounded-full w-3/5 mx-auto" src="/images/volunteer1.jpg" alt="volunteer-1" />
+                            <h2 className="text-center text-gray-800 text-4xl my-4">Melissa</h2>
                         </div>
                         <div className="my-3 mx-2">
-                            <img className="image-container" src="/images/volunteer2.jpg" alt="volunteer2" />
-                            <h2 className="text-center text-black">Djack</h2>
+                            <img className="image-team rounded-full w-3/5 mx-auto" src="/images/volunteer2.jpg" alt="volunteer2" />
+                            <h2 className="text-center text-gray-800 text-4xl my-4">Djack</h2>
                         </div>
                         <div className="my-3 mx-2">
-                            <img className="image-container" src="/images/volunteer3.jpg" alt="volunteer-3" />
-                            <h2 className="text-center text-black">Paulo</h2>
+                            <img className="image-team rounded-full w-3/5 mx-auto" src="/images/volunteer3.jpg" alt="volunteer-3" />
+                            <h2 className="text-center text-gray-800 text-4xl my-4">Paulo</h2>
                         </div>
                         <div className="my-3 mx-2">
-                            <img className="image-container" src="/images/volunteer4.jpg" alt="volunteer-4" />
-                            <h2 className="text-center text-black">Ivan</h2>
+                            <img className="image-team rounded-full w-3/5 mx-auto" src="/images/volunteer4.jpg" alt="volunteer-4" />
+                            <h2 className="text-center text-gray-800 text-4xl my-4">Ivan</h2>
                         </div>
                         <div className="my-3 mx-2">
-                            <img className="image-container" src="/images/volunteer5.jpg" alt="volunteer-5" />
-                            <h2 className="text-center text-black">Solange</h2>
+                            <img className="image-team rounded-full w-3/5 mx-auto" src="/images/volunteer5.jpg" alt="volunteer-5" />
+                            <h2 className="text-center text-gray-800 text-4xl my-4">Solange</h2>
                         </div>
                         <div className="my-3 mx-2">
-                            <img className="image-container" src="/images/volunteer6.jpg" alt="volunteer-6" />
-                            <h2 className="text-center text-black">Fernanda</h2>
+                            <img className="image-team rounded-full w-3/5 mx-auto" src="/images/volunteer6.jpg" alt="volunteer-6" />
+                            <h2 className="text-center text-gray-800 text-4xl my-4">Fernanda</h2>
                         </div>
                         <div className="my-3 mx-2">
-                            <img className="image-container" src="/images/volunteer7.jpg" alt="volunteer-6" />
-                            <h2 className="text-center text-black">Ivania</h2>
+                            <img className="image-team rounded-full w-3/5 mx-auto" src="/images/volunteer7.jpg" alt="volunteer-6" />
+                            <h2 className="text-center text-gray-800 text-4xl my-4">Ivania</h2>
                         </div>
                         <div className="my-3 mx-2">
-                            <img className="image-container" src="/images/volunteer8.jpg" alt="volunteer-6" />
-                            <h2 className="text-center text-black">Marco</h2>
+                            <img className="image-team rounded-full w-3/5 mx-auto" src="/images/volunteer8.jpg" alt="volunteer-6" />
+                            <h2 className="text-center text-gray-800 text-4xl my-4">Marco</h2>
                         </div>
                         <div className="mt-3 mx-2 mb-5">
-                            <img className="image-container" src="/images/volunteer9.jpg" alt="volunteer-6" />
-                            <h2 className="text-center text-black">Fortunato</h2>
+                            <img className="image-team rounded-full w-3/5 mx-auto" src="/images/volunteer9.jpg" alt="volunteer-6" />
+                            <h2 className="text-center text-gray-800 text-4xl my-4">Fortunato</h2>
                         </div>
                     </div>
                 </div>

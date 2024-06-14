5330563555d219c5df17f21391db2ec936a902ef
jacam666 <ja6cam@gmail.com>
2024-06-14 14:44:19 +0100

    homepage styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,21 +16,25 @@ export default function Home() {
       <ResponsiveNavbar />
       <div className=" pb-8">
         <div className="text-center py-9">
-          <h1 className="homepage-header text-6xl md:text-7xl text-gray-900 text-center mx-auto md:w-3/4 lg:w-2/3 lg:text-8xl py-9">Building a Brighter Future for Cape Verde</h1>
-          <h3 className="text-2xl lg:text-4xl py-5 sm:py-9">Join us in making a difference, one community at a time.</h3>
+          <h1 className="homepage-header text-6xl md:text-7xl text-gray-900 text-center mx-auto md:w-3/4 lg:w-2/3 lg:text-6xl mt-24">Building a Brighter Future for Cape Verde</h1>
+          <h3 className="text-2xl lg:text-2xl sm:py-9">Join us in making a difference, one community at a time.</h3>
         </div>
         <HomePageButton />
       </div>
-      <h1 className="homepage-header text-5xl md:text-6xl  text-gray-900 text-center mx-auto my-5 px-5 py-9">
-        We are a welfare charity aiming to help overcome poverty in Cape Verde and improve the quality of life for Cape Verdeans.
-      </h1>
-      {/* <div className="flex flex-col sm:flex-row w-full justify-between "> */}
+      <div className=" mx-4 rounded-lg text-center py-4 my-5">
+        <h1 className="homepage-header text-gray-900">
+          My Cape Verde
+        </h1>
+        <p className="text-xl px-5 text-gray-800 text-center sm:w-1/2 mx-auto">
+          We are a welfare charity aiming to help overcome poverty in Cape Verde and improve the quality of life for Cape Verdeans.
+        </p>
+      </div>
       <div className="flex flex-col sm:flex-row">
-        <div className="flex flex-col sm:w-1/3">
+        <div className="flex flex-col sm:w-1/5">
           <p className="homepage-header text-xl font-bold text-gray-900 mt-8  px-4">Why Choose Us?</p>
           <div className=" my-4 ml-6 h-1 mb-8 sm:h-1 w-2/6 lg:w-2/5 md:w-2/5 bg-black "></div>
         </div>
-        <div className="sm:w-1/3 my-3 flex flex-col justify-center items-center">
+        <div className="sm:w-2/5 my-3 flex flex-col justify-center items-center">
           <Image
             src="/images/frontpageimage1_edited.jpg"
             alt="gallery"
@@ -45,9 +49,7 @@ export default function Home() {
             Empowering local communities sustainably.
           </p>
         </div>
-
-
-        <div className="sm:w-1/3 flex flex-col justify-center items-center">
+        <div className="sm:w-2/5 flex flex-col justify-center items-center">
           <Image
             src="/images/Whats App Image 2024-04-27 at 17_edited2.jpg"
             alt="gallery"
@@ -62,11 +64,19 @@ export default function Home() {
             Improving access to essential healthcare for all.
           </p>
         </div>
-
-
       </div>
-      {/* </div> */}
+
       <div className=" h-1 sm:h-1 w-1/2 lg:w-2/5 md:w-2/5 bg-black mx-auto my-4"></div>
+
+      <div className=" mx-4 rounded-lg text-center py-4 ">
+        <h1 className="homepage-header text-gray-900 ">
+          Be Part of the Change
+        </h1>
+        <p className="text-xl sm:w-1/2 text-center mx-auto text-gray-700">
+          Your support can make all the difference. Join us in our mission to empower and uplift our communities.
+        </p>
+        <HomePageButton />
+      </div>
       <Footer />
     </div>
   );
